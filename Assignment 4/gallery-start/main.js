c282158ae342ee44d637a4d07b097e85be2c33dd
adammkama <156977023+adammkama@users.noreply.github.com>
2024-08-14 17:52:48 -0400

    Update main.js
    added alt texts for the images, made for loop for the images that sets the src and alt text for each object and adds an event listener that reads a click to set the displayed image, added darken/lighten button that checks to see if the buttons class is in lighten or dark and sets an overlay on the images to dim or lighten them

--- Assignment 4/gallery-start/main.js
+++ Assignment 4/gallery-start/main.js
@@ -8,13 +8,40 @@ const overlay = document.querySelector('.overlay');
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-
+const alts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'A beautiful landscape',
+    'pic3.jpg': 'A serene beach',
+    'pic4.jpg': 'A bustling cityscape',
+    'pic5.jpg': 'A majestic mountain'
+};
 
 /* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    // event listener
+    newImage.addEventListener('click', function (e) {
+        displayedImage.setAttribute('src', newImage.getAttribute('src'));
+        displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+    });
+}
+
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', function () {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    }
+}); 
