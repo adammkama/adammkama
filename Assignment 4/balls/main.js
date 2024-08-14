351f2966ab3326ff1e719c427a16f0f107e294af
adammkama <156977023+adammkama@users.noreply.github.com>
2024-08-14 18:28:44 -0400

    Update main.js
    added collision detection method in ball class and added a line to call the method in the loop

--- Assignment 4/balls/main.js
+++ Assignment 4/balls/main.js
@@ -57,9 +57,24 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
     }
+
+// collision detection
+    collisionDetect() {
+        for (const ball of balls) {
+            if (this !== ball) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+        
+                if (distance < this.size + ball.size) {
+                ball.color = this.color = randomRGB();
+                }
+            }
+        }
+    }
   
       
-  }
+}
   
 // test ball
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
@@ -95,6 +110,7 @@ function loop() {
     for (const ball of balls) {
       ball.draw();
       ball.update();
+      ball.collisionDetect();
     }
   
     requestAnimationFrame(loop);
